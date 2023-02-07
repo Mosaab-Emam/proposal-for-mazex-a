923ef0f4f0dfe5741df1d54016e1ef0833cd4e73
Mosaab_Emam <mosaab.emam123@gmail.com>
2023-02-07 09:54:38 +0200

    Opt out of tailwind

--- remotion.config.ts
+++ remotion.config.ts
@@ -2,10 +2,8 @@
 // Each option also is available as a CLI flag: https://remotion.dev/docs/cli
 // ! The configuration file does only apply if you render via the CLI !
 
-import { enableTailwind } from 'app/remotion/enable-tailwind';
 import { Config } from 'remotion';
 
 Config.setImageFormat('jpeg');
 Config.setOverwriteOutput(true);
 Config.setEntryPoint('app/remotion/index.ts');
-Config.overrideWebpackConfig(enableTailwind);
