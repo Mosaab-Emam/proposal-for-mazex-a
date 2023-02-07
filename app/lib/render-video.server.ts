5db4c4576943f721f12262e6cd2a806c7e76a56c
Mosaab Emam <mosaab.emam123@gmail.com>
2023-02-07 13:20:36 +0000

    Snaps don't work on gitpod

--- app/lib/render-video.server.ts
+++ app/lib/render-video.server.ts
@@ -20,39 +20,39 @@ export const renderVideo = async ({
 }): Promise<RenderResponse> => {
 	console.log("At least I am here");
 
-	const entry = "../remotion/index.ts";
+	const entry = "app/remotion/index.ts";
 
 	console.log("Creating a Webpack bundle of the video");
-  const bundleLocation = await bundle(path.resolve(entry), () => undefined, {
-    // If you have a Webpack override, make sure to add it here
-    webpackOverride: (config: any) => config,
-  });
+	const bundleLocation = await bundle(path.resolve(entry), () => undefined, {
+		// If you have a Webpack override, make sure to add it here
+		webpackOverride: (config: any) => config,
+	});
 
 	const comps = await getCompositions(bundleLocation, {
-    // You can pass custom input props that you can retrieve using getInputProps()
-    // in the composition list. Use this if you want to dynamically set the duration or
-    // dimensions of the video.
-    inputProps,
-  });
+		// You can pass custom input props that you can retrieve using getInputProps()
+		// in the composition list. Use this if you want to dynamically set the duration or
+		// dimensions of the video.
+		inputProps,
+	});
 
 	// Select the composition you want to render.
-  const composition2 = comps.find((c) => c.id === COMPOSITION_ID);
- 
-  // Ensure the composition exists
-  if (!composition2) {
-    throw new Error(`No composition with the ID ${COMPOSITION_ID} found.
+	const composition2 = comps.find((c) => c.id === COMPOSITION_ID);
+
+	// Ensure the composition exists
+	if (!composition2) {
+		throw new Error(`No composition with the ID ${COMPOSITION_ID} found.
   Review "${entry}" for the correct ID.`);
-  }
+	}
 
 	console.log("Attempting to render:", outName);
-  await renderMedia({
-    composition2,
-    serveUrl: bundleLocation,
-    codec: "h264",
-    outName,
-    inputProps,
-  });
-  console.log("Render done!");
+	await renderMedia({
+		composition2,
+		serveUrl: bundleLocation,
+		codec: "h264",
+		outName,
+		inputProps,
+	});
+	console.log("Render done!");
 
 	const region = process.env.REMOTION_AWS_REGION as AwsRegion | undefined;
 	if (!region) {
